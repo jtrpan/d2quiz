da2eb66e8ff23485956dcc2e807a62d25a29161b
jtrpan <james.t.pan@yandex.com>
2020-09-19 18:32:12 -0400

    some image change implmented. stable version.

---
+++ build/precache-manifest.e79896ff1be5bc8b34889f3a6975f10d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24241c8cd7144a771a52d78f758fd3f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc2daa34bbe55425956a",
+    "url": "/static/css/2.5c0c5824.chunk.css"
+  },
+  {
+    "revision": "ac80a580a2fa22281a34",
+    "url": "/static/css/main.31d04972.chunk.css"
+  },
+  {
+    "revision": "bc2daa34bbe55425956a",
+    "url": "/static/js/2.3d50c4d2.chunk.js"
+  },
+  {
+    "revision": "412ab6df2f08dd32e5451092aec88976",
+    "url": "/static/js/2.3d50c4d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac80a580a2fa22281a34",
+    "url": "/static/js/main.3a17325a.chunk.js"
+  },
+  {
+    "revision": "1f03006faf414e187939",
+    "url": "/static/js/runtime-main.2d6b9f96.js"
+  }
+]);

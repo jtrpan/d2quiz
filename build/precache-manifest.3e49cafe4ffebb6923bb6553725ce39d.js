c7d4268a605d8859abaabf0f0f7fd12c9e47d9f4
JmesPan <James.Pan@Alumni.UBC.ca>
2021-02-27 13:37:10 -0500

    Added patch for react-quiz-component.

---
+++ build/precache-manifest.3e49cafe4ffebb6923bb6553725ce39d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e775ab08e2aa810d6d2e1dfb36010a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0dfda25742ef41c0db0b",
+    "url": "/static/css/2.352b67cf.chunk.css"
+  },
+  {
+    "revision": "2ef96a4f3c76f43dc35c",
+    "url": "/static/css/main.31d04972.chunk.css"
+  },
+  {
+    "revision": "0dfda25742ef41c0db0b",
+    "url": "/static/js/2.182adc7b.chunk.js"
+  },
+  {
+    "revision": "b45e53aa2e1075fda9d20fa3f5e00781",
+    "url": "/static/js/2.182adc7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ef96a4f3c76f43dc35c",
+    "url": "/static/js/main.c407e169.chunk.js"
+  },
+  {
+    "revision": "1f03006faf414e187939",
+    "url": "/static/js/runtime-main.2d6b9f96.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bd45dbd09c6c509fb32a85661fa677ac.js"
+  "/precache-manifest.3e49cafe4ffebb6923bb6553725ce39d.js"
 );
 
 self.addEventListener('message', (event) => {
